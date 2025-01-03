a7cc325b17213a1a95402ac121bee855c74ab339
DavidHabakkuk <davidhabakkuk984@gmail.com>
2025-01-03 16:37:00 +0100

    Fix configuration for Next.js and Tailwind CSS.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "tailwindcss";
 
-export default {
+const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -13,10 +13,11 @@ export default {
         foreground: "var(--foreground)",
       },
       animation: {
-        
-        'spin-slow': 'spin 15s linear infinite',
+        "spin-slow": "spin 60s linear infinite",
       },
     },
   },
   plugins: [],
-} satisfies Config; 
+};
+
+export default config;
