d49a3d61addd2859c6e6aefbc879a09d1d964f65
DavidHabakkuk <davidhabakkuk984@gmail.com>
2025-01-28 18:48:10 +0100

    Fix configuration issues

--- next.config.ts
+++ next.config.ts
@@ -10,19 +10,18 @@ const config: Config = {
     extend: {
       colors: {
         background: "var(--background)",
-        foreground: "var(--foreground)",
+        foreground: "var(--foreground)", 
         navy: {
           customNavy: "#04033f",
         },
       },
       animation: {
-        "spin-slow": "spin 60s linear infinite",
-        blink: "blink 1.2s step-start infinite",
+        "spin-slow": "spin 60s linear infinite", 
+        blink: "blink 1.2s step-start infinite", 
       },
       keyframes: {
         blink: {
-          "0%, 100%": { opacity: "1" },
-          "50%": { opacity: "0" },
+          
         },
       },
     },
