b8cd01bcc40758446e2654d3bd6efa9d1bcc3ba6
DavidHabakkuk <davidhabakkuk984@gmail.com>
2025-01-28 18:22:02 +0100

    Fix configuration issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Media from "./component/media";
 import Hero from "./component/Hero";
 import About from "./component/About";
 import Team from "./component/Team";
-import Project from "./component/project";
+import Project from "./component/project/project";
 import Skill from "./component/skill";
 import Contact from "./component/contact";
 import Footer from "./component/Footer";
