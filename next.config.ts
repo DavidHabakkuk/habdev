478278a4731ee1acfe52018aaef286a11f74a88d
DavidHabakkuk <davidhabakkuk984@gmail.com>
2025-02-02 10:08:03 +0100

    next error

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,13 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // Add other Next.js configuration options here
+  keyframes: {
+    blink: {
+      "0%, 100%": { opacity: 1 },
+      "50%": { opacity: 0 },
+    },
+  },
+  
 };
 
 export default nextConfig;
