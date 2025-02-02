bb4efd62b7acfe0ab4499dac1a32449ac9a13302
DavidHabakkuk <davidhabakkuk984@gmail.com>
2025-02-02 18:05:14 +0100

    try to solving next.config issueas

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
+import type { NextConfig } from 'next';
+
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-
-  keyframes: {
-    blink: {
-      
-    },
-  },
+ 
 };
+
+export default nextConfig;
