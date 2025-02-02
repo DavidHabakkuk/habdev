f1dc2e79355638e9be522a095e0597bf7db1419d
DavidHabakkuk <davidhabakkuk984@gmail.com>
2025-02-02 17:47:23 +0100

    Logo name changed

--- src/app/component/Hero.tsx
+++ src/app/component/Hero.tsx
@@ -2,6 +2,8 @@
 
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
+import Link from "next/link"
+import { LiaCalendarCheckSolid } from "react-icons/lia";
 
 const texts = [
   "Welcome to <span class='text-blue-500'>HabDev</span>,",
@@ -41,15 +43,18 @@ const Hero: React.FC = () => {
   return (
     <section className="min-h-screen flex flex-col items-center justify-between bg-gray-900 text-white relative">
       <div className="fixed top-4 left-4 md:top-6 md:left-20 z-30 flex items-center gap-3 animate-fade-in-down">
-        <Image
+       <LiaCalendarCheckSolidink href="/">
+       <Image
           src="/images/hab.png"
           alt="HabDev Logo"
           width={50}
           height={50}
           className="rounded-full shadow-lg"
         />
+       </Link>
+
         <span className="text-2xl md:text-3xl font-bold bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600 text-transparent bg-clip-text tracking-wider">
-          Hab<span className="text-white">Dev</span>
+          Hab<span className="text-white">Tech</span>
         </span>
       </div>
       <div className="flex flex-col items-center text-center px-6 mt-20 md:mt-32 lg:mt-40 animate-fade-in">
