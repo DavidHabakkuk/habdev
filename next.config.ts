d32ae0645a72d37ef66741e239a3ff3cec44d0e5
DavidHabakkuk <davidhabakkuk984@gmail.com>
2025-02-02 10:00:49 +0100

    next error

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  
+  swcMinify: true,
+  // Add other Next.js configuration options here
 };
 
 export default nextConfig;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,27 +1,28 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)", 
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
         navy: {
-          customNavy: "#04033f",
+          customNavy: '#04033f',
         },
       },
       animation: {
-        "spin-slow": "spin 60s linear infinite", 
-        blink: "blink 1.2s step-start infinite", 
+        'spin-slow': 'spin 60s linear infinite',
+        blink: 'blink 1.2s step-start infinite',
       },
       keyframes: {
         blink: {
-          
+          '0%, 100%': { opacity: '1' },
+          '50%': { opacity: '0' },
         },
       },
     },
