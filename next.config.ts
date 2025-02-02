749226c5bae870438a8fdef63f07c5f86df39e71
DavidHabakkuk <davidhabakkuk984@gmail.com>
2025-02-02 10:51:09 +0100

    error fixing

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,11 @@
-import type { NextConfig } from 'next';
-
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  // Other configurations...
   keyframes: {
     blink: {
-      "0%, 100%": { opacity: 1 },
-      "50%": { opacity: 0 },
+      '0%, 100%': { opacity: '1' },
+      '50%': { opacity: '0' },
     },
   },
-  
 };
-
-export default nextConfig;
